5790123f6e0528d6cf5c89180b764fec38d210f0
Iliya Brook <iliyabrook1987@gmail.com>
2025-04-19 20:27:41 +0300

    admin page

--- shared-types/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './token.types';
-export * from './mongoose.types';
-export * from './auth.types';
-export * from './user.types';
-export * from './common.types';
-export * from './post.types';
-export * from './shortlink.types';
